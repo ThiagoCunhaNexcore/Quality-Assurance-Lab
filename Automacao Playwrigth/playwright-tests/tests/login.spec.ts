6f8c8ba9f1366d9854d059b7692bbce2fb206230
Thiago Phelip da Silva Cunha <thiagophelipcunha@gmail.com>
2026-02-06 10:15:15 -0300

    hj

---
+++ Automacao Playwrigth/playwright-tests/tests/login.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('deve permitir login com credenciais válidas', async ({ page }) => {
+  await page.goto('/login');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('teste@teste.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('heading', { name: 'Login realizado' }).click();
+  await page.getByText('Olá, teste@teste.com').click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
+
+
